19b40b7ba3186edcfdaeb36ba6dd15b6a58fe7f5
Jornada de Dados <robert.j.almeida1803@gmail.com>
2024-11-23 21:46:30 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Jornada de dados")
+
+# Variáveis 
+idade = 34
+nome = "Luciano"
+
+
+# Print 
+print(idade)
+print(nome)
+
+# Operações 
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista  
+Lista = [1,2,3,4,5]
+
+# Métodos
+Lista_invertida = Lista.reverse()
